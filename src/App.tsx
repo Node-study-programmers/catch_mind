83d49ed740be4d15796f02ec13231675c6695171
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-05-23 19:55:36 +0900

    frontend_setting

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-slate-500 text-white flex justify-center items-center text-3xl">
+      Catch Mind!
+    </div>
+  );
+}
+
+export default App;
