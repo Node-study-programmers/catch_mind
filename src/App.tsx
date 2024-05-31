14f83a2c17f34b534902ab988573ce44ce5bb3f0
nyh <nsi3358@naver.com>
2024-05-31 19:23:10 +0900

    PR TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import img from "./asset/img/qwe.jpg";
+import img from './asset/img/qwe.jpg';
 function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-300">
       {/* <h1 className="text-4xl font-bold text-mainColor font-mainFont">
         Hello, Tailwind CSS with Gradients!
       </h1> */}
+      PR TEST
       <img src={img} alt="" className="w-[50vw] h-[50vh]" />
     </div>
   );
