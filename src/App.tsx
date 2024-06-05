97269b677274b4221f7c4a5c3527f6c75deb5b47
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-06-06 02:21:31 +0900

    [feature] : routing 및 404페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import AuthPage from "./pages/AuthPage";
+import InGame from "./pages/InGame";
+import MyPage from "./pages/MyPage";
+import Rank from "./pages/Rank";
+import NotFound from "./pages/NotFound";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/auth",
+      element: <AuthPage />,
+    },
+    // 인게임 라우팅에서 방 입장해서 대기하는 페이지랑 게임 진행시 페이지 같게 해야 하는지??
+    {
+      path: "/ingame/:roomId",
+      element: <InGame />,
+    },
+    {
+      path: "/mypage",
+      element: <MyPage />,
+    },
+    {
+      path: "/rank",
+      element: <Rank />,
+    },
+  ]);
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-300"></div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
