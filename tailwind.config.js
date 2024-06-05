8da3451f1be9b50c2061e0e6e77a5ae5ae130268
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-06-05 15:37:32 +0900

    base

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,24 @@ export default {
         kakao: "#FEE500", //카카오 배경색
         rankText: "#00738C",
         board: "#FFFFFF", //보드 배경색(화이트)
-        subBoard: "#F2F2F2", //보드 위의 서브 보드(회색)
+        subBoard: "whitesmoke", //보드 위의 서브 보드(회색)
         //사이드바
-        notActiveText: "#006B82",
+        notActiveText: "#00738C",
         activeText: "#FFB800", //사이드바 테두리도 동일(주황)
+
+        // 방 컨테이너
+        roomContainer: " ",
+        roomBoxshadow: "",
+        roomTitle: "",
+        roomContent: "",
+
+        // 인게임
+        userContainer: "#E2F2FD",
+        timerContainer: "#E2F2FD",
+        timerText: "black",
+        timerEmergency: "red",
+        inGame: "#ff5454",
+        waitingGame: "#91f74f",
       },
       fontFamily: {
         titleFont: ["Schoolbell", "sans-serif"],
