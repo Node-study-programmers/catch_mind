564d58b89f7c40eb4ac9c0a92f9f249813a24752
nyh <nsi3358@naver.com>
2024-06-12 23:51:30 +0900

    STYLE : 랭킹 디자인 뼈대 구성

---
+++ src/components/Rank/RankContainer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Props {
+  profileUrl: string;
+  nickname: string;
+  totalScore: number;
+  ranking: number;
+}
+
+const RankContainer = ({ profileUrl, nickname, totalScore, ranking }: Props) => {
+  return (
+    //각각 1의 비율로 정렬
+    <tr className="grid grid-cols-4 items-center justify-center bg-white border rounded-2xl">
+      <td className="text-center p-2">{ranking}</td>
+      <td className="flex justify-center p-2">
+        <img src={profileUrl} alt="" className="h-20 w-20 object-cover rounded-full" />
+      </td>
+      <td className="text-center p-2">{nickname}</td>
+      <td className="text-center p-2">{totalScore}</td>
+    </tr>
+  );
+};
+
+export default RankContainer;
