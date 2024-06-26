b904b9948dba3e612e85c4564c56df0e014b61f4
nyh <nsi3358@naver.com>
2024-06-26 18:08:45 +0900

    FEAT : 로딩창 구현

--- src/components/modal/AlertModal.tsx
+++ src/components/modal/AlertModal.tsx
@@ -4,9 +4,10 @@ interface Props {
   open: boolean;
   handleClose: () => void;
   message: string;
+  isLoadingAlert?: boolean;
 }
 
-const AlertModal = ({ open, handleClose, message }: Props) => {
+const AlertModal = ({ open, handleClose, message, isLoadingAlert }: Props) => {
   const style = {
     position: "absolute",
     top: "50%",
@@ -21,12 +22,35 @@ const AlertModal = ({ open, handleClose, message }: Props) => {
     p: 4,
   };
 
+  const loadingStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    borderRadius: "20px",
+    transform: "translate(-50%, -50%)",
+    display: "flex",
+    justifyContent: "center",
+    bgcolor: "background.paper",
+    boxShadow: 24,
+    p: 4,
+    fontWeight: "700",
+  };
+
+  if (isLoadingAlert) {
+    return (
+      <Modal open={open} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+        <Box sx={loadingStyle}>{message}</Box>
+      </Modal>
+    );
+  }
+
   return (
     <Modal
       open={open}
       onClose={handleClose}
       aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description">
+      aria-describedby="modal-modal-description"
+    >
       <Box sx={style}>
         <Typography id="modal-modal-title" variant="h6" component="h2">
           {message}
