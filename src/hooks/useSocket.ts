7d84513bc41b5269418cc84ea6760c2bc15b7621
nyh <nsi3358@naver.com>
2024-06-27 23:22:36 +0900

    STYLE : 문자열 '' 컨벤션 변경

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react';
-import io, { Socket } from 'socket.io-client';
+import { useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const socket = io(import.meta.env.VITE_API_URL, { path: '소켓 연결 경로' }); // 소켓연결
+    const socket = io(import.meta.env.VITE_API_URL, { path: "소켓 연결 경로" }); // 소켓연결
     setSocket(socket);
 
-    socket.on('대기 이벤트', data => {
+    socket.on("대기 이벤트", data => {
       //이벤트에 맞는 로직...
     });
 
@@ -19,7 +19,7 @@ export const useSocket = () => {
 
   //채팅 보내는 이벤트
   const submitChat = (data: any) => {
-    socket?.emit('chat', data);
+    socket?.emit("chat", data);
   };
 
   return { submitChat };
