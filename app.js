586226be5a74929dc54977329c50c609a0eadf9f
chansik0504 <chansik0504@gmail.com>
2024-07-01 18:50:21 +0900

    socket test3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-const socketIo = require('socket.io');
+const socketIo = require('./socket.js');
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -9,6 +9,7 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
+
 
 // 서버 생성
 const server = http.createServer(app);
@@ -39,24 +40,7 @@ app.use('/home', homeRouter);
 // app.use('/game', gameRouter);
 // app.use('/room', roomRouter);
 
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  // 기본 메시지 이벤트
-  socket.on('message', (message) => {
-      console.log('Received message:', message);
-      socket.emit('message', 'Message received: ' + message);
-  });
-
-  // 연결 해제 이벤트
-  socket.on('disconnect', () => {
-      console.log('A user disconnected');
-  });
-});
-
-
 const PORT = process.env.PORT || 9999; // 포트 설정을 이 줄로 이동
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- socket.js
+++ socket.js
@@ -1,44 +1,74 @@
-// const socketIo = require('socket.io');
-// const jwt = require('jsonwebtoken');
+const socketIo = require('socket.io');
+const User = require('./models/User');
+const Room = require('./models/Room');
 
-// module.exports = (server) => {
-//     const io = socketIo(server);
-//     io.use((socket, next) => {
-//         const token = socket.handshake.query.token;
-//         if (token) {
-//             jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//                 if (err) {
-//                     return next(new Error('토큰 에러'));
-//                 }
-//                 socket.user = decoded;
-//                 next();
-//             });
-//         } else {
-//             return next(new Error('토큰이 없습니다'));
-//         }
-//     });
+module.exports = (server) => {
+    const io = socketIo(server, {path : '/socket.io', cors: { origin: '*'}});
 
-//     io.on('connection', (socket) => {
-//         console.log(`${socket.user.nickname}님 서버 연결`);
+    io.use(async (socket, next) => {
+        const email = socket.handshake.query.email;
+        if (email) {
+            try {
+                const user = await User.findOne({ email: email });
+                if (!user) {
+                    return next(new Error('유저 정보가 없습니다.'));
+                }
+                socket.user = user; // 사용자 정보를 소켓에 저장합니다.
+                next();
+            } catch (err) {
+                return next(new Error('DB 에러'));
+            }
+        } else {
+            next(new Error('유저 정보가 없습니다.'));
+        }
+    });
 
-//         // 방 입장
-//         socket.on('joinRoom', (room) => {
-//             socket.join(room);
-//             console.log(`${socket.user.nickname}님이 ${room}에 입장하셨습니다.`);
-//         })
+    io.on('connection', (socket) => {
+        console.log(`${socket.user.nickname}님 서버 연결`);
 
-//         // 메세지 전송
-//         socket.on('sendMessage', (data) => {
-//             io.to(data.room).emit('message', `${socket.user.nickname}: ${data.message}`);
-//         })
+        // 방 입장
+        socket.on('joinRoom', async (roomId) => {
+            try {
+                const room = await Room.findById(roomId);
+                if (!room) {
+                    return socket.emit('error', '방이 존재하지 않습니다.');
+                }
 
-//         socket.on('leaveRoom', (room) => {
-//             socket.leave(room);
-//             console.log(`${socket.user.nickname}님이 ${room}에서 퇴장하셨습니다.`);
-//         })
+                socket.room = room;
+                socket.join(socket.room.id);
+                console.log(`${socket.user.nickname}님이 ${socket.room.name}에 입장하셨습니다.`);
+                io.to(socket.room.id).emit('message', `${socket.user.nickname}님이 입장하셨습니다.`);
+            } catch (err) {
+                console.error('Room 입장 중 에러:', err);
+                socket.emit('error', '방에 입장하는 중 에러가 발생했습니다.');
+            }
+        });
 
-//         socket.on('disconnect', () => {
-//             console.log(`${socket.user.nickname}님 서버 연결 해제`);
-//         })
-//     });
-// }
+        // 메세지 전송
+        socket.on('sendMessage', (data) => {
+            const messageData = {
+                message: data.message,
+                nickname: socket.user.nickname,
+            }
+            io.to(data.room).emit('message', messageData);
+        })
+
+        socket.on('leaveRoom', async () => {
+            try {
+                socket.leave(socket.room.id);
+                console.log(`${socket.user.nickname}님이 ${socket.room.name}에서 퇴장하셨습니다.`);
+                io.to(socket.room.id).emit('message', `${socket.user.nickname}님이 퇴장하셨습니다.`);
+            } catch (err) {
+                console.error('Room 입장 중 에러:', err);
+                socket.emit('error', '방에 입장하는 중 에러가 발생했습니다.');
+            }
+        });
+
+        socket.on('disConnect', () => {
+            console.log(`${socket.user.nickname}님 서버 연결 해제`);
+            socket.leave(socket.room.id);
+            io.to(socket.room.id).emit('message', `${socket.user.nickname}님이 퇴장하셨습니다.`);
+            socket.room = null;
+        });
+    });
+}
