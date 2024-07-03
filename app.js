3c756adb1bb0106d21a4bd5443043d79ef250239
chansik0504 <chansik0504@gmail.com>
2024-07-03 18:03:41 +0900

    배포 test2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const socketIo = require('./socket.js');
 const dotenv = require("dotenv");
 dotenv.config();
 
+console.log(1);
+
 const express = require("express");
 const connectMongoDB = require('./models/index');
 const app = express();
@@ -12,6 +14,7 @@ const http = require('http');
 const initialWords = require('./words');
 const Word = require('./models/word');
 
+console.log(2);
 
 // 서버 생성
 const server = http.createServer(app);
@@ -20,11 +23,17 @@ const io = socketIo(server);
 connectMongoDB();
 app.use(cors());
 
+console.log(3);
+
+
 // Image 폴더가 없으면 생성
 const imageFolder = path.join(__dirname, 'profileImages');
 if (!fs.existsSync(imageFolder)) {
   fs.mkdirSync(imageFolder);
 }
+
+console.log(4);
+
 
 const initializeDatabase = async () => {
   const count = await Word.countDocuments();
@@ -43,6 +52,9 @@ const initializeDatabase = async () => {
   }
 };
 
+console.log(5);
+
+
 app.use('/profileImages', express.static(path.join(__dirname, '../profileImages')));
 
 const authRouter = require('./routes/auth');
@@ -50,10 +62,15 @@ const mypageRouter = require('./routes/mypage');
 const rankRouter = require('./routes/rank');
 const homeRouter = require('./routes/home');
 
+console.log(6);
+
+
 app.use('/auth', authRouter);
 app.use('/mypage', mypageRouter);
 app.use('/rank', rankRouter);
 app.use('/home', homeRouter);
+
+console.log(7);
 
 
 const PORT = process.env.PORT || 9999; // 포트 설정을 이 줄로 이동
