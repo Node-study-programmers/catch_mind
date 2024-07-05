6b35d312998e16f3d7c9d2141d55ed1dd9cf78e7
chansik0504 <chansik0504@gmail.com>
2024-07-05 19:08:49 +0900

    배포

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const io = socketIo(server);
 
 connectMongoDB();
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://6687c55a96539900087603fc--catchmind-program.netlify.app'
 }));
 
 // Image 폴더가 없으면 생성
